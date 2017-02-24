af73866107e84df90de4cb7a353f2cdd20b5ce5e
FarofaScript <debuga@farofascript.com.br>
2017-02-24 00:35:25 -0500

    Atualiza exemplos da Farofa Session 7

--- fs7.js
+++ fs7.js
@@ -1,9 +1,34 @@
+/*
+Função que não retorna valor.
+*/
+var logar = texto => console.log(texto);
+logar('teste');
+
+/*
+Função que retorna valor primitivo.
+*/
+var calcular = (num1, num2) => num1 + num2;
+calcular(1, 2);
+
+/*
+Função que retorna objeto.
+*/
+var obterCliente = id => ({
+        id: id,
+        nome: 'Cliente 1'
+    });
+
+console.log(obterCliente(1));
+
+/*
+Função parte de um objeto
+*/
 var cliente = {
+    id: 1,
     nome: 'Cliente 1',
-    idade: 30,
-    descricao(msg) {
-        return msg;
+    descricao() {
+        return 'Nome: ' + this.nome + ' (id: ' + this.id + ')';
     }
 };
 
-console.log(cliente.descricao('teste'));
+console.log(cliente.descricao());
